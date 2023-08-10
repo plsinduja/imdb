66372199683abf73d5bfd49d2762497f92ae38b0
plsinduja <polisettysindhuja@gmail.com>
2023-08-10 15:52:16 +0300

    add dark mode switch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "./DarkModeSwitch";
+import Link from "next/link";
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
@@ -8,11 +10,16 @@ export default function Header() {
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div>
-        <h2 className="text-2xl">
-          <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
-          <span className="text-xl hidden sm:inline">Clone</span>
-        </h2>
+      <div className="flex space-x-5 items-center">
+        <DarkModeSwitch />
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
       </div>
     </div>
   );
